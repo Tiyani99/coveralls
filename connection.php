eb6bd1075e311770f498f39718262251e4b2a3b1
sakhile22 <22sakhile@gmail.com>
2020-07-30 00:21:08 +0200

    New changes

--- connection.php
+++ connection.php
@@ -2,7 +2,7 @@
 
 $db_name = "sakhile";
 $username = "root";
-$password = "s@kh!lE22";
+$password = "Sm*22^03%#";
 $server_name = "localhost";
 
 $connection = mysqli_connect($server_name, $username, $password, $db_name);
